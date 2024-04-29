39dc9131281738b21720903de6538548d93a9aa8
HongJingi <jinx.hong@samsung.com>
2024-04-29 11:10:45 +0900

    [feature] page 7

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -6,7 +6,15 @@ public class Game {
         if (guessNumber.equals(question)) {
             return new GuessResult(true, 3, 0);
         } else {
-            return new GuessResult(false, 0, 0);
+            int strikes = 0;
+            for (int i = 0; i < question.length(); i++) {
+                char charAt = guessNumber.charAt(i);
+                int indexOf = question.indexOf(charAt);
+                if (indexOf == i) {
+                    strikes++;
+                }
+            }
+            return new GuessResult(false, strikes, 0);
         }
     }
 
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -47,6 +47,12 @@ class GameTest {
         assertMatchedNumber(game.guess("456"), false, 0, 0);
     }
 
+    @Test
+    void returnUnSolvedResultIfSomeMatchedNumber() {
+        generateQuestion("123");
+        assertMatchedNumber(game.guess("120"), false, 2, 0);
+    }
+
     private void generateQuestion(String questionNumber) {
         game.question = questionNumber;
     }
