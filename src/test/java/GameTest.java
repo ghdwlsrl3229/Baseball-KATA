6ec9402fec5babdc6ccef5eb02f6b4a35ca44d4a
HongJingi <jinx.hong@samsung.com>
2024-04-29 10:59:24 +0900

    [feature] page 5

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,6 +1,8 @@
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
@@ -34,5 +36,15 @@ class GameTest {
         assertIllegalArgument("121");
     }
 
+    @Test
+    void returnSolvedResultIfMatchedNumber() {
+        game.question = "123";
+        GuessResult result = game.guess("123");
 
+        assertThat(result).isNotNull();
+        assertThat(result.solved).isEqualTo(true);
+        assertThat(result.strikes).isEqualTo(3);
+        assertThat(result.balls).isEqualTo(0);
+
+    }
 }
