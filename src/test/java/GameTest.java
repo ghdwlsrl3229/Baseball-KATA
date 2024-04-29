580b6b37102a19335b29355ab868ce074abfca1a
HongJingi <jinx.hong@samsung.com>
2024-04-29 11:02:53 +0900

    [feature] page 6

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -45,6 +45,16 @@ class GameTest {
         assertThat(result.isSolved()).isEqualTo(true);
         assertThat(result.getStrikes()).isEqualTo(3);
         assertThat(result.getBalls()).isEqualTo(0);
+    }
 
+    @Test
+    void returnSolvedResultIfUnmatchedNumber() {
+        game.question = "123";
+        GuessResult result = game.guess("456");
+
+        assertThat(result).isNotNull();
+        assertThat(result.isSolved()).isEqualTo(false);
+        assertThat(result.getStrikes()).isEqualTo(0);
+        assertThat(result.getBalls()).isEqualTo(0);
     }
 }
